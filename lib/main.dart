87ec52658aca8c8cf3b87da0f4446f86e9d2e8fc
Shih-Yuan Lee (FourDollars) <sylee@canonical.com>
2022-04-27 20:36:51 +0800

    Refine onDetect function.

--- lib/main.dart
+++ lib/main.dart
@@ -32,32 +32,35 @@ class CIDHolder extends StatefulWidget {
 
 class _CIDHolderState extends State<CIDHolder> {
   MobileScannerController cameraController = MobileScannerController();
-  var appBarTitleText = Text('CID Holder');
+
+  String _title = 'CID Holder';
+
+  void _onDetect(qrcode, args) {
+    setState(() {
+      if (qrcode.rawValue == null) {
+          debugPrint('Failed to scan qrcode');
+      } else {
+        final String code = qrcode.rawValue!;
+        if (code.startsWith('https://certification.canonical.com/hardware/')) {
+          var parts = code.split('/');
+          _title = 'CID Holder ${parts[4]}';
+          debugPrint('CID found! ${parts[4]}');
+        } else if (!code.isEmpty) {
+          debugPrint('qrcode found! $code');
+        }
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: appBarTitleText),
+      appBar: AppBar(title: Text('$_title')),
       body: MobileScanner(
-          allowDuplicates: false,
-          controller: MobileScannerController(
-            facing: CameraFacing.back, torchEnabled: true),
-          onDetect: (qrcode, args) {
-            if (qrcode.rawValue == null) {
-              debugPrint('Failed to scan qrcode');
-            } else {
-              final String code = qrcode.rawValue!;
-              if (code.startsWith('https://certification.canonical.com/hardware/')) {
-                var parts = code.split('/');
-                debugPrint('CID found! ${parts[4]}');
-                setState(() {
-                  appBarTitleText = Text('CID Holder ${parts[4]}');
-                });
-              } else {
-                debugPrint('qrcode found! $code');
-              }
-            }
-          }),
+        controller: MobileScannerController(
+          facing: CameraFacing.back,
+          torchEnabled: true),
+        onDetect: _onDetect),
     );
   }
 }
